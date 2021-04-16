4c50e59d00224ee69a9862de6b50e44c4b53e47e
NathanGonzalez44 <nathangonzalez@outlook.fr>
2021-04-16 11:49:28 +0200

    inital commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ff52d2143e7ca413724307c7c8705b1d'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd96b94a0-f4d8-4c63-95cf-28be7612a0b2'
+};
